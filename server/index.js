18124af681391f870d55ec61e32fc7f772d8272e
Ivan Popelyshev <ivan.popelyshev@gmail.com>
2020-08-07 20:47:32 +0300

    server auth test

--- server/index.js
+++ server/index.js
@@ -1,9 +1,33 @@
 const http = require("http");
 const express = require("express");
+const uuid = require("uuid");
+const md5 = require("md5");
+const fs = require('fs');
+
+const api_secret = 'VmGsge58F2kL6TOCoHnq';
 
 const app = express();
 
 app.use("/api", express.json());
+
+class User {
+    constructor(options) {
+        const {token, viewer_id} = options || {};
+        this.token = token || uuid();
+        this.viewer_id = viewer_id || 0;
+    }
+}
+
+function getUser(req) {
+    const {viewer_id, api_id, auth_key} = req.query;
+    const check_auth_key = md5(api_id + '_' + viewer_id + '_' + api_secret);
+
+    if (auth_key !== check_auth_key) {
+        return false;
+    }
+    return new User({viewer_id});
+}
+
 
 app.get("/api", (req, res)=>{
     res.send({
@@ -11,9 +35,26 @@ app.get("/api", (req, res)=>{
     })
 });
 
+function uglyTemplate(req, res) {
+    const usr = getUser(req);
+    if (!usr) {
+        res.send(403);
+        return;
+    }
+
+    fs.readFile(__dirname +'/../dist/index.html', 'utf8', (err, data) => {
+        res.send(data);
+    });
+}
+
+app.get("/", uglyTemplate);
+app.get("/index.html", uglyTemplate);
+
 app.use("/", express.static(__dirname + "/../dist" ));
 
 const server = http.createServer(app);
 server.listen(2345, "0.0.0.0", ()=>{
     console.log("Server started on:", 8080);
 });
+
+// https://localhost:2345/?api_id=7558520&viewer_id=1787423&auth_key=ef85ea88588c32d3dd4230d7ac040c05
